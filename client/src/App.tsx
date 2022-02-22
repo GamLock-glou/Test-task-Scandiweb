ff27c7d910d7bad196916c9ca8222066e10edb5c
GamLock-glou <klimenkov.z@mail.ru>
2022-02-23 00:38:55 +0300

    End

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -92,7 +92,6 @@ class App extends React.Component<Record<string, never>, AppState> {
   }
 
   setProductsCart = (productId: string, attributes: {}, prices: Price[]) => {
-    //shit
     let product = this.state.productsInCart.find((product) => product.productId == productId)
     let count = product ? product.productCount + 1 : 1;
     let newProduct = { productId: productId, productCount: count, attributes: attributes, prices: prices }

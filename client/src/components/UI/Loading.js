a2edd105dfd755ccf993933612153f7a52237695
GamLock-glou <klimenkov.z@mail.ru>
2022-03-08 20:31:20 +0300

    add Loading and PageNotFound

---
+++ client/src/components/UI/Loading.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import s from './Model.module.css';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className={s.myLoading}>
+            Loading...
+      </div>
+    );
+  }
+}
+
+export default Loading;
---
+++ client/src/components/UI/PageNotFound.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import s from './Model.module.css';
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <div
+        className={s.error}
+      >
+              Page not found
+      </div>
+    );
+  }
+}
+
+export default PageNotFound;
